6ae12043efb82a760df37e77ebd4d3e16ada881f
Daniel Treasure <dbtreasure@gmail.com>
2014-07-20 16:32:19 -0600

    added new logic to handle responses from 30 days ago with pagination

--- app/scripts/services/github.js
+++ app/scripts/services/github.js
@@ -10,23 +10,35 @@
 angular.module('trackerApp')
   .service('github', function github($http, $q) {
     this.commits = function(){
+      console.log('this.commits was called')
       var defer = $q.defer();
-      var oneWeekAgo = moment().subtract('days', 7).format();
-
-      var handleApiResponse = function(response){
-        var numOfCommits = 0;
-        var createEventsAndPushEvents = [];
+      var oneMonthAgo = moment().subtract('days', 31).format();
+      var generateNextUrl = function(headers){
+        var link = headers('link');
+        var arr = link.split('>');
+        return arr[0].substring(1);
+      }
+      var numOfCommits = 0;
+      var handleApiResponse = function(response, status, headers, config){
         response.forEach(function(el, index){
-          if(el.type === "PushEvent" && el.payload.ref || el.type === "CreateEvent" && el.payload.ref){
+          var dateOfEvent = moment(el.created_at);
+          if(el.type == "PushEvent" && dateOfEvent.isAfter(oneMonthAgo)){
             numOfCommits++
-            createEventsAndPushEvents.push(el);
+          }
+
+          if(index == 29){
+            var lastEvent = moment(el.created_at)
+            if(lastEvent.isAfter(oneMonthAgo)){
+              $http.get(generateNextUrl(headers)).success(handleApiResponse);
+            } else{
+                defer.resolve(numOfCommits);
+            }
+
           }
         })
-        console.log(createEventsAndPushEvents);
-        defer.resolve(numOfCommits);
       };
 
-      var url = 'https://api.github.com/users/dantreasure/events/public?page=1&per_page=100';
+      var url = 'https://api.github.com/users/dantreasure/events?access_token=ba63c6bf7947444e4189567d7554dc6f6ffb5c92';
 
       $http.get(url).success(handleApiResponse);
 
